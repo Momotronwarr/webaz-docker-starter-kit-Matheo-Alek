5bee30f9a470dd328168325810d8348ab163b807
Alek De Oliveira <alek.deoliveira@gmail.com>
2025-11-08 18:48:00 +0100

    a

--- Projet_Sevran/core-master/views/menu.php
+++ Projet_Sevran/core-master/views/menu.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projet Sevran (KAARIS le boss)</title>
+    <link rel="stylesheet" href="assets\Menu_CSS.css">    
 </head>
 <body>
-    kaaris
+    <div id="titre">
+        <p>Sevran 2</p>
+    <div>
+    <div> 
+        <p>Bienvenue sur le jeu de Sevran (on modifiera apers on mettera un vrai texte)</p>
+    </div>
+    <div>Pour mieux vous impreniez de l'ambiance de Sevran voici quelques vidéos plus ou moins connues...</div>
+    <a href="https://www.youtube.com/watch?v=ukiP_Ykq2_A"  target=_blank> Todiefor & Kaaris - SEVRAN <a>
+    <a href="https://youtu.be/n5nE7Qr8p48?si=1nzZnXegEKLSNl3j"  target=_blank> Todiefor & Kaaris - SEVRAN <a>
+
+    https://youtu.be/n5nE7Qr8p48?si=1nzZnXegEKLSNl3j
 </body>
 </html>
 
