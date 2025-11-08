68c0818ca2107472cb4cfca39f35c4dccdc685eb
Alek De Oliveira <alek.deoliveira@gmail.com>
2025-11-08 18:59:49 +0100

    a

--- Projet_Sevran/core-master/views/menu.php
+++ Projet_Sevran/core-master/views/menu.php
@@ -9,15 +9,19 @@
 <body>
     <div id="titre">
         <p>Sevran 2</p>
-    <div>
+    </div>
     <div> 
         <p>Bienvenue sur le jeu de Sevran (on modifiera apers on mettera un vrai texte)</p>
     </div>
     <div>Pour mieux vous impreniez de l'ambiance de Sevran voici quelques vidéos plus ou moins connues...</div>
-    <a href="https://www.youtube.com/watch?v=ukiP_Ykq2_A"  target=_blank> Todiefor & Kaaris - SEVRAN <a>
-    <a href="https://youtu.be/n5nE7Qr8p48?si=1nzZnXegEKLSNl3j"  target=_blank> Todiefor & Kaaris - SEVRAN <a>
-
-    https://youtu.be/n5nE7Qr8p48?si=1nzZnXegEKLSNl3j
+    <div id = "video"> 
+        <p> Présentation musicale de Sevran </p>
+        <p><a href="https://www.youtube.com/watch?v=ukiP_Ykq2_A"  target=_blank> Todiefor & Kaaris - SEVRAN <a></p>
+        <p><a href="https://youtu.be/n5nE7Qr8p48?si=1nzZnXegEKLSNl3j"  target=_blank> clip lourd !! Rgt - Sevran <a></p>
+        <p>Enfin mon préféré : Kaaris qui montre enfin ses émotions</p>
+        <p><a href="https://youtu.be/yAoPhl4LaWg?si=jReyGI3TvQbWdzvj"  target=_blank> Kaaris x VIDEO CLUB - ROI DE SEVRAN <a></p>
+    </div>
+    
 </body>
 </html>
 
