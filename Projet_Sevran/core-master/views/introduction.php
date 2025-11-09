d7dea76b4b60c1e192e3e6db38a5c5cfc5a8a2cb
Alek De Oliveira <alek.deoliveira@gmail.com>
2025-11-09 22:22:47 +0100

    a

--- Projet_Sevran/core-master/views/introduction.php
+++ Projet_Sevran/core-master/views/introduction.php
@@ -7,39 +7,81 @@
     <link rel="stylesheet" href="assets\CSS\Introduction.css">   
 </head>
 <body>
-    <div>Pour mieux vous impreniez de l'ambiance de Sevran voici quelques vidéos plus ou moins connues...</div>
-    
+    <h1>Pour mieux vous impreniez de l'ambiance de Sevran voici quelques vidéos, photos plus ou moins connues...</h1>
+        
+    <div id ="titre">
+        <p><strong>Présentation musicale de Sevran</strong></p>
+    </div>
+
     <div id = "video"> 
 
-        <p><strong>Présentation musicale de Sevran</strong></p>
-
         <a href="https://www.youtube.com/watch?v=ukiP_Ykq2_A" target="_blank">
-            <img src="https://img.youtube.com/vi/ukiP_Ykq2_A/hqdefault.jpg" alt="Todiefor & Kaaris - SEVRAN" width="400">
+            <img src="https://img.youtube.com/vi/ukiP_Ykq2_A/hqdefault.jpg" alt="Todiefor & Kaaris - SEVRAN" width="400" heigth="225">
         </a>
-        <p>Todiefor & Kaaris - SEVRAN</p>
+
         <a href="https://youtu.be/n5nE7Qr8p48?si=1nzZnXegEKLSNl3j"  target=_blank> 
-            <img src="https://img.youtube.com/vi/n5nE7Qr8p48/hqdefault.jpg" alt="Rgt - Sevran" width="400">
+            <img src="https://img.youtube.com/vi/n5nE7Qr8p48/hqdefault.jpg" alt="Rgt - Sevran" width="400" heigth="225">
         </a>
-        <p>clip lourd !! Rgt - Sevran</p>
-        <p>Enfin mon préféré : Kaaris qui montre enfin ses émotions</p>
         <a href="https://www.youtube.com/watch?v=yAoPhl4LaWg" target="_blank">
-            <img src="https://img.youtube.com/vi/yAoPhl4LaWg/hqdefault.jpg" alt="Kaaris x VIDEO CLUB - ROI DE SEVRAN" width="400">
+            <img src="https://img.youtube.com/vi/yAoPhl4LaWg/hqdefault.jpg" alt="Kaaris x VIDEO CLUB - ROI DE SEVRAN" width="400" heigth="225">
         </a>
-        <p>Kaaris x VIDEO CLUB - ROI DE SEVRAN</p>
 
     </div>
-    <div id="images">
+
+    <div id ="texte1">
+        <p>Todiefor & Kaaris - SEVRAN</p>
+        <p>clip lourd !! Rgt - Sevran</p>
+        <p>Kaaris montre enfin ses émotions !!! : Kaaris x VIDEO CLUB - ROI DE SEVRAN</p>
+    </div>
+
+    <div id ="titre">
+        <p><strong>Viens découvrir des endroits géniaux à Sevran</strong></p>
+    </div>
+
+    <div id="images1">
+        <a href="https://www.google.com/maps/place/Sevran+-+Livry/@48.9360649,2.5322589,17z/data=!3m1!4b1!4m6!3m5!1s0x47e6140b09c275af:0x5dba1549983a6aec!8m2!3d48.9360614!4d2.5348338!16s%2Fg%2F120lwswx?entry=ttu&g_ep=EgoyMDI1MTEwNC4xIKXMDSoASAFQAw%3D%3D" target="_blank">
+            <p>
+                <Img src=assets/images/gare-sevran-beaudottes.webp alt = "Gare de Sevran" width="400" heigth="225">
+            </p>
+        </a>
+        <a href="https://www.google.com/maps/place/La+Corne+d'Or/@48.9392196,2.5325341,15.5z/data=!4m10!1m2!2m1!1sla+corne+d'or!3m6!1s0x47e61474b7046561:0x1998825e02299fe0!8m2!3d48.9368244!4d2.5313071!15sCg1sYSBjb3JuZSBkJ29yWg8iDWxhIGNvcm5lIGQgb3KSARJ0dXJraXNoX3Jlc3RhdXJhbnSqAUgQASoRIg1sYSBjb3JuZSBkIG9yKAQyHhABIhoDfIxxnIZcj7voW9LxU1h-8-vVDESKpjfJBzIREAIiDWxhIGNvcm5lIGQgb3LgAQA!16s%2Fg%2F11c6t8m9pj?entry=ttu&g_ep=EgoyMDI1MTEwNC4xIKXMDSoASAFQAw%3D%3D" target="_blank">
+            <p>
+                <Img src=assets/images/la_corne_d_or.webp alt = "La Corne d'Or" width="400" heigth="225">
+            </p>
+        </a>
+        <a href="https://www.google.com/maps/search/quartier+beaudottes+sevran/@48.9467925,2.5205357,18z/data=!3m1!4b1?entry=ttu&g_ep=EgoyMDI1MTEwNC4xIKXMDSoASAFQAw%3D%3D" target="_blank">
+            <p>
+                <Img src=assets/images/bedotte.webp alt = "Les bedottes" width="400" heigth="225">
+            </p>
+        </a>
+    </div>
+
+    <div id="texte2" >
         <p>Célèbre gare de Sevran (un RER B de compétition !)</p>
-        <p><Img src=assets/images/gare-sevran-beaudottes.webp alt = "Gare de Sevran" width="400" heigth="225"></p>
-        <p>Le très connu Pascal Sevran</p>
-        <p><Img src=assets/images/Pascal_Sevran.webp alt = "Pascal Sevran" width="400" heigth="225"></p>
-        <p>Enfin la fameuse Corne d'Or !</p>
-        <p><Img src=assets/images/la_corne_d_or.webp alt = "La Corne d'Or" width="400" heigth="225"></p>
+        <p>La fameuse Corne d'Or !</p><p>La cité les Bedottes à Sevran</p>
+    </div>
+
+    <div id ="titre">
+        <p><strong>Iconne de Sevran</strong></p>
+    </div>
+
+    
+
+    <div id = "images2">
+        
+        <p><Img src=assets/images/Pascal_Sevran.webp alt = "Pascal Sevran" width="500" heigth="250"></p>
+        
+        <p><Img src=assets/images/kaaris.webp alt = "Pascal Sevran" width="500" heigth="250"></p>
+    </div>
+    
+    <div id = "texte3">
+        <p>Pascal Sevran</p>
+        <p>Kaaris</p>
     </div>
 
     <form action="/map" method="get">
         <button type="submit" title="Map">Doro Party ?! (Traduction : Prêt à découvrir Sevran ?!)</button>
-        <input id="triche" type="checkbox"> <label for="triche">Mode des tricheurs (Looser 👎​)</label>
+        <input id="triche" type="checkbox"> <label for="triche">Mode des tricheurs (Pour les Looser 👎​)</label>
     </form>
     
 </body>
